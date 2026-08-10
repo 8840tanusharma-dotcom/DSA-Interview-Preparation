0ba488831a3e6e79ba4dfb803fe2423790e9f75a
Tanu Sharma <8840tanusharma@gmail.com>
2026-08-10 12:03:27 +0530

    Time: 0 ms (100%), Space: 42.2 MB (20.76%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int gcd = getGCD(a, b);
+        int count=0;
+       
+       for(int i=1;i<=gcd;i++){
+        if(gcd%i==0){
+            count++;
+        }
+       }       
+       return count;
+    }
+    private int getGCD(int x,int y){
+        while(y!=0){
+            int temp=y;
+            y=x%y;
+            x=temp;
+        }
+        return x;
+    }
+}
